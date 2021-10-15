2b72d8485eabb5d01589cf46d4d063e801712d8b
Bryan <bvacher@com-company.fr>
2021-10-15 11:56:15 +0200

    Correction

--- src/main/java/org/example/volunteers/Personne.java
+++ src/main/java/org/example/volunteers/Personne.java
@@ -18,44 +18,4 @@ public class Personne {
         adresseMail = tableau[3];
         tel = tableau[4];
     }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getPseudo() {
-        return pseudo;
-    }
-
-    public void setPseudo(String pseudo) {
-        this.pseudo = pseudo;
-    }
-
-    public String getAdresseMail() {
-        return adresseMail;
-    }
-
-    public void setAdresseMail(String adresseMail) {
-        this.adresseMail = adresseMail;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
 }
