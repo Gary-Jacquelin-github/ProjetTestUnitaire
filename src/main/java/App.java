ce78889ad29211bee46c7101bb1e939015c400a6
Crofto <tanguyreymbaut@gmail.com>
2021-10-15 11:25:07 +0200

    Personne attributs et instantiation dans app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -10,6 +11,11 @@ public class App {
         List<String[]> lines = Files.readAllLines(Paths.get("src/main/resources/data.csv"))
             .stream().map(string -> string.split(";"))
             .collect(toList());
+
+        List<Personne> personnes = new ArrayList<>();
+        for (String[] line : lines) {
+            personnes.add(new Personne (line));
+        }
 
         // Apply dark magic here...
 
--- src/main/java/Personne.java
+++ src/main/java/Personne.java
@@ -7,8 +7,14 @@ public class Personne {
     public String adresseMail;
     public String tel;
 
+    public Personne(){}
+
     public Personne(String[] tableau){
         nom = tableau[0];
+        prenom = tableau[1];
+        pseudo = tableau[2];
+        adresseMail = tableau[3];
+        tel = tableau[4];
     }
 
 }
