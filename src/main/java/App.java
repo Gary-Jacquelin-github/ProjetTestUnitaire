d451cc0f785bc66102defef116cdb2839ce13bc4
Gary Jacquelin <gary.jacquelin@energy-go.fr>
2021-10-15 14:20:02 +0200

    Premier test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,9 +11,10 @@ import static java.util.stream.Collectors.toList;
 public class App {
     public static void main(String[] args) throws IOException {
         List<String[]> lines = Files.readAllLines(Paths.get("src/main/resources/data.csv"))
-            .stream().map(string -> string.split(";"))
+            .stream().map(string -> string.split(";", -1))
             .collect(toList());
 
+        //Instantiation des personnes
         List<Personne> personnes = new ArrayList<>();
         for (String[] line : lines) {
             personnes.add(new Personne (line));
@@ -24,7 +25,19 @@ public class App {
             per.formater();
         }
 
-        // Apply dark magic here...
+        //On boucle sur tous
+        for (int i=0 ; i< personnes.size() ; i++){
+            //On check les doublons et on rempli tmtc
+            for (int j=0 ; j< personnes.size() ; j++){
+                //On regarde si c'est un doublons
+                if(personnes.get(i).isDoublon(personnes.get(j))){
+                    //On complete le tt
+                    personnes.get(i).complete(personnes.get(j));
+                    //On le remove pour ne pas repasser dessus et donc enlever les doublons
+                    personnes.remove(j);
+                }
+            }
+        }
 
         System.out.println("Result goes here");
     }
--- src/test/java/testParse.java
+++ src/test/java/testParse.java
@@ -1,5 +1,14 @@
+import org.example.volunteers.Utils;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class testParse {
+    @Test
+    public static void testParseTel(){
+        String telTest = "+33085552814";
+        String telObtenu = Utils.conversionTel("telTest");
+        assertEquals("085552814", telObtenu);
+    }
 }
