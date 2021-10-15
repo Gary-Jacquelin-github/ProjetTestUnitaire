21afe31caf03dd58bb80d4683e3bc85878e7fb45
Crofto <tanguyreymbaut@gmail.com>
2021-10-15 12:23:14 +0200

    Personne : rectification betises brian

--- src/main/java/org/example/volunteers/Personne.java
+++ src/main/java/org/example/volunteers/Personne.java
@@ -18,4 +18,34 @@ public class Personne {
         adresseMail = tableau[3];
         tel = tableau[4];
     }
+    /**
+     * Regarde si pour un objet donné, l'objet en parametre est un doublon
+     * @param personne personne qui est un doublon
+     * @return si c'est un doublon
+     */
+    public boolean isDoublon(Personne personne){
+        return this.tel.equals(personne.tel) || this.adresseMail.equals(personne.adresseMail) || this.pseudo.equals(personne.pseudo);
+    }
+
+    /**
+     * Complete l'objet personne en fonction du supposé doublon en parametre
+     * @param personne doublon qui doit completer l'objet
+     */
+    public void complete(Personne personne){
+        if( this.nom.equals("")){
+            this.nom = personne.nom;
+        }
+        if( this.prenom.equals("")){
+            this.prenom = personne.prenom;
+        }
+        if( this.pseudo.equals("")){
+            this.prenom = personne.prenom;
+        }
+        if( this.adresseMail.equals("")){
+            this.adresseMail = personne.adresseMail;
+        }
+        if( this.tel.equals("")){
+            this.tel = personne.tel;
+        }
+    }
 }
